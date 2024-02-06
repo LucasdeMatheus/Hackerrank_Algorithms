fd0e3ceba9f18dc064ffd0f5ff948801a15ed165
Lucas Matheus <ffmatheus6@GMAIL.COM>
2024-02-06 15:12:20 -0300

    sla

---
+++ numberLineJumps.js
@@ -0,0 +1,13 @@
+function kangaroo(x1, v1, x2, v2) {
+    if(x2 > x1 && v2 > v1){
+        return 'NO'
+    }else if(x2 < x1 && v2 < v1){
+        return 'NO'
+    }
+    if((x1 - x2) % (v1 - v2) == 0){
+        return 'YES'
+    } else {
+        return "NO"
+    }
+}
+console.log(kangaroo(0, 3, 4, 2))
